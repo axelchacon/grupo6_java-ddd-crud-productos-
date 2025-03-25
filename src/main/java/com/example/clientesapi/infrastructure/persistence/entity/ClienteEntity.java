f1ec0815e8b86affb408ebf2ea04c5afedb870c9
Grecia Delgado <greciadm2596@gmail.com>
2025-03-24 20:33:02 -0500

    Add new model in to client domain

--- src/main/java/com/example/clientesapi/infrastructure/persistence/entity/ClienteEntity.java
+++ src/main/java/com/example/clientesapi/infrastructure/persistence/entity/ClienteEntity.java
@@ -28,11 +28,11 @@ public class ClienteEntity {
     @Id
     private UUID id;
 
-    @Column(nullable = false, unique = true, length = 50)
-    private String codigo;
-
     @Column(nullable = false, length = 100)
     private String nombre;
+
+    @Column(nullable = false, length = 100)
+    private String apellido;
 
     @Column(columnDefinition = "TEXT")
     private String descripcion;
